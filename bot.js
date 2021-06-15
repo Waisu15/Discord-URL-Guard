726758aa9e5efea37ce77606c9b222674fe29a00
Waisu <85125438+Waisu242@users.noreply.github.com>
2021-06-15 19:50:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on("guildUpdate", async (olds, news) => {
 
 if(olds.vanityURLCode ==! news.vanityURLCode) {
 let logs = await news.fetchAuditLogs({type: "GUILD_UPDATE"}).then(audit => audit.entries.first());
-let member = newGuild.member(newGuild.members.cache.get(logs.entries.first().executor.id));
+let member = newGuild.member(newGuild.members.cache.get(logs.executor.id));
 if(member || member.id ==! config.BOT.admin || member.id ==! client.user.id) {
 
 let ch = client.channels.cache.get(config.SETTİNGS.log);
